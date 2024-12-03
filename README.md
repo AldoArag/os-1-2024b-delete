e8bd9cc376659fea7e9e348b5d1a965d51e08b2c
AldoArag <dorasoaragon66@gmail.com>
2024-12-03 03:05:14 +0000

    proyecto final

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# os-1-2024b-delete
-Proyecto final de la materia de Sistemas Operativos I 2024B 
+<center> <img src="./md/images/itgam_cover.png" atl="Itgam banner"> </center>
+
+# 06-Sistemas-Operativos-I
+
+## 🧾Descripción
+
+Repositorio de la materia de sistemas operativos I
+
+## 👤Autor
+
+[Aldo Aragon](https://github.com/AldoArag)
+
+## 🌿 Ramas del Proyecto
+
+[🏆 S04E99 Proyecto Final](https://github.com/AldoArag/os-1-2024b-delete/tree/s04e99-processes-lab)
--- projects/process-state.js
+++ projects/process-state.js
@@ -1,3 +1,14 @@
 export function processState(){
-    console.log("== Estado de procesos ===");
+    console.log("===  Estado de procesos  ===");
+
+    // Obteniendo informacion del proceso actual
+    console.log(`📢 ID del proceso: ${process.pid}`);
+    console.log(`📢 Memory Usage: ${JSON.stringify(process.memoryUsage(),null, '\t') }`);
+    console.log(`📢 CPU Time: ${JSON.stringify(process.cpuUsage(), null,'\t')}`);
+
+    // Eventos del proceso
+    // Evento close
+    process.on('exit', (code) => {
+        console.log(`➡️ Proceso finaliza con el código: ${code}`)
+    });
 }
